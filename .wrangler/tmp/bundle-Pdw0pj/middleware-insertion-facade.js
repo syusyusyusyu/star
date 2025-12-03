edfc9e0c1052c283dd7391b954be9df1dafd482f
syusyusyusyu <c2375@oic.jp>
2025-12-03 17:25:45 +0900

    Refactor code structure for improved readability and maintainability

---
+++ .wrangler/tmp/bundle-Pdw0pj/middleware-insertion-facade.js
@@ -0,0 +1,11 @@
+				import worker, * as OTHER_EXPORTS from "C:\\Users\\cafec\\OneDrive\\Desktop\\勉強用\\star\\worker\\index.ts";
+				import * as __MIDDLEWARE_0__ from "C:\\Users\\cafec\\OneDrive\\Desktop\\勉強用\\star\\node_modules\\wrangler\\templates\\middleware\\middleware-ensure-req-body-drained.ts";
+import * as __MIDDLEWARE_1__ from "C:\\Users\\cafec\\OneDrive\\Desktop\\勉強用\\star\\node_modules\\wrangler\\templates\\middleware\\middleware-miniflare3-json-error.ts";
+
+				export * from "C:\\Users\\cafec\\OneDrive\\Desktop\\勉強用\\star\\worker\\index.ts";
+
+				export const __INTERNAL_WRANGLER_MIDDLEWARE__ = [
+					
+					__MIDDLEWARE_0__.default,__MIDDLEWARE_1__.default
+				]
+				export default worker;

1bcff196b49f20877a7b48faf2d043ff92b765e6
shu_yoshida <C2375@oic.jp>
2026-01-13 22:17:31 +0900

    fix: スクリプトの参照を更新し、楽曲情報の表示を改善

--- src/pages/IndexPage.tsx
+++ src/pages/IndexPage.tsx
@@ -278,8 +278,8 @@ export default function IndexPage() {
                         <div className="text-left">
                             <p className="text-[10px] text-gray-500 tracking-widest">POWERED BY TEXTALIVE</p>
                             <p className="text-[10px] text-gray-500 tracking-widest">SONGLE</p>
-                            <p className="text-[10px] text-gray-500 mt-1">「ストリートライト」 加賀（ネギシャワーP）<br>
-                            原曲：piapro（https://piapro.jp/t/ULcJ）｜許諾済 </br></p>
+                            <p className="text-[10px] text-gray-500 mt-1">「ストリートライト」 加賀（ネギシャワーP）<br />
+                            原曲：piapro（https://piapro.jp/t/ULcJ）｜許諾済</p>
                         </div>
                     </div>
 
